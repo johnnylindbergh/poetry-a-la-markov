feb65615c9c418b57b24691698281d2881c9e84c
johnnylindbergh <johnnyl8@embarqmail.com>
2019-07-28 21:24:22 -0400

    Markov package

--- markov.js
+++ markov.js
@@ -2,8 +2,6 @@
 	markov.js: Markov chain bits 
 */
 
-const markov = require('string-markov-js');
-
 /*
 	
 	Ngram -> word
@@ -13,3 +11,36 @@ const markov = require('string-markov-js');
 	Ngram -> array of all possible consequent words
 
 */
+
+var markov = require('string-markov-js');
+
+const rhyming_schemes = 
+[
+	"a b a b",
+	"a b a b b c b c",
+	"a b c b c d e b e d"
+]
+
+var dataset = markov.newDataSet();
+
+var line_length = 10;
+
+var filename = 'bee_movie_script.txt';
+var ngram = 3;
+var preserveLineBreaks = true;
+ 
+dataset.trainOnFile(filename, ngram, preserveLineBreaks, function() {
+    console.log("Training complete.");
+});
+
+var startWithCapitalNGram = true;
+var text = dataset.generate(100, startWithCapitalNGram);
+
+
+
+module.exports = {
+
+	markov_single:function(){
+
+	}
+}
