552e1a3c9edcb98bb9b34f517bcab4ba38a55657
johnnylindbergh <johnnyl8@embarqmail.com>
2019-07-28 22:54:40 -0400

    begining to integrate markov object and rhyme scheme

--- markov.js
+++ markov.js
@@ -16,25 +16,37 @@ var markov = require('string-markov-js');
 
 const rhyming_schemes = 
 [
-	"a b a b",
+	"a b / a b",
 	"a b a b b c b c",
 	"a b c b c d e b e d"
 ]
 
+var rhyming_scheme = rhyming_schemes[0];
 var dataset = markov.newDataSet();
 
 var line_length = 10;
 
 var filename = 'bee_movie_script.txt';
-var ngram = 3;
+var ngram = 2;
 var preserveLineBreaks = true;
 
 dataset.trainOnFile(filename, ngram, preserveLineBreaks, function() {
     console.log("Training complete.");
 
 
-    var text = dataset.generate(75, true);
-    console.log(text);
+   var text = ""
+   rhyming_scheme = rhyming_scheme.split(" ")
+   for (var line = 0; line < rhyming_scheme.length; line++) {
+   	if (rhyming_scheme[line] == "/"){
+   		console.log("new stanza");
+   	} else {
+   		console.log("new line with rhyme", rhyming_scheme[line])
+   			}
+   }
+
+   var possibilities = dataset.getPossibilities(['This','is']);
+
+    console.log(possibilities);
 });
 
 
