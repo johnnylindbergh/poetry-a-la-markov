cbc5b6f906619e84108f09ce98db97e7f9657067
Thomas Castleman <thomas7rex@gmail.com>
2019-08-14 12:32:55 -0400

    Remove from generate.js

--- generate.js
+++ generate.js
@@ -1,36 +1,12 @@
 /*
-	generate.js: Processing of corpus
+	generate.js: Highest-level of abstraction for generating poetry
 */
 
 module.exports = {
 
+	/*	Generate a string poem, given a trained markov chain object, and a
+		string representing the desired rhyme scheme */
 	generatePoemFromScheme: function(chain, scheme) {
-		/*
-			Scheme: A B / A B
-		*/
-
-		/*
-			used_rhyming_words = []
-			tokens = {}
-
-			For each token in scheme:
-				if token not already used
-					Generate line up until last word
-					Get last word purely from chain, ensuring word does not rhyme with any used rhyming word in ANOTHER token
-
-					get all rhymes with this word, and add them to tokens object:
-						"<token>" --> [words that rhyme]
-				else
-					Generate line until last word
-					Get markov possibilities for last word, get rhyme possibilities for this token (cached in tokens object)
-
-					Generate their intersection, preserving frequency from markov possibilities
-					
-					If intersection non-empty:
-						Choose random last word from intersection
-					else
-		*/
-
 
 	}
 
