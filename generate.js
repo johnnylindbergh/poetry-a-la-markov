831b5ef79aaa0e7bc42a9f4438a7cfc9a704580f
Thomas Castleman <thomas7rex@gmail.com>
2019-08-14 12:24:11 -0400

    I was doing something

--- generate.js
+++ generate.js
@@ -5,12 +5,30 @@
 module.exports = {
 
 	generatePoemFromScheme: function(chain, scheme) {
-
+		/*
+			Scheme: A B / A B
+		*/
 
 		/*
-			For each line:
-				Find corresponding rhyme
-				
+			used_rhyming_words = []
+			tokens = {}
+
+			For each token in scheme:
+				if token not already used
+					Generate line up until last word
+					Get last word purely from chain, ensuring word does not rhyme with any used rhyming word in ANOTHER token
+
+					get all rhymes with this word, and add them to tokens object:
+						"<token>" --> [words that rhyme]
+				else
+					Generate line until last word
+					Get markov possibilities for last word, get rhyme possibilities for this token (cached in tokens object)
+
+					Generate their intersection, preserving frequency from markov possibilities
+					
+					If intersection non-empty:
+						Choose random last word from intersection
+					else
 		*/
 
 
